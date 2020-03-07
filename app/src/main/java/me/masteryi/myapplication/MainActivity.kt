8a00fb1aae5d477e4263aeaea5b8298ae93b31bc
Charlie Lee <jon_ly@163.com>
2020-03-07 17:07:46 +0800

    init

---
+++ app/src/main/java/me/masteryi/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package me.masteryi.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.masteryi.myapplication.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+
+        }
+    }
+}
